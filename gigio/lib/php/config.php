43a4ffba8b911b4b484d25a335d1382ae403585b
hpollack <hermann.pollack@gmail.com>
2017-01-20 23:53:39 -0300

    chaos

---
+++ gigio/lib/php/config.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+====================================================
+Parámetros de configuracion
+====================================================
+
+Estos son requeridos para el funcionamiento global de los archivos del sistema
+Se deben cambiar a posteriori con los parámetros reales
+*/
+
+//Ruta del sistema. 
+$url = "http://localhost/gigio/";
+
+//Parámetros de la base de datos
+$host = "localhost";
+$user = "root";
+$pas  = "hermann";
+$bd   = "recabarius";
+
+
+
+ ?>
